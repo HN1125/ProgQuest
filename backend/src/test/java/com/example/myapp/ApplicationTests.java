41fdf91713bd16d66258ed1ed75ef7533e90d94a
HN1125 <h.numata1125@gmail.com>
2025-07-22 00:27:59 +0900

    テストクラスの修正

--- backend/src/test/java/com/example/myapp/ApplicationTests.java
+++ backend/src/test/java/com/example/myapp/ApplicationTests.java
@@ -1,10 +1,10 @@
-package myapp.demo;
+package com.example.myapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
